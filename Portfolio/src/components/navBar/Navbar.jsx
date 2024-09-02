90d083ccdf241b1a0451a741482aba49c03259c0
Naman Mishra <125729522+Naman-Mishra-3116@users.noreply.github.com>
2024-09-02 16:48:35 +0530

    updated the logo

--- Portfolio/src/components/navBar/Navbar.jsx
+++ Portfolio/src/components/navBar/Navbar.jsx
@@ -1,13 +1,17 @@
 import React from "react";
-import { logo } from "../../assets/images";
 import { navLinksdata } from "./../../constants/index";
+import { TbBrandNetflix } from "react-icons/tb";
+import { SiStreamlit } from "react-icons/si";
 import { Link } from "react-scroll";
 
 const Navbar = () => {
   return (
     <div className="w-full h-24 mx-auto px-16 flex sticky top-0 z-50 bg-bodyColor justify-between items-center font-titleFont border-b-[1px] border-b-gray-600">
-      <div>
-        <img src={logo} alt="logo" />
+      <div className="rounded-full">
+        <span className="w-16 h-16 bg-black bg-opacity-25 text-gray-200 text-xl flex flex-col items-center justify-center rounded-full shadow-sm hover:bg-opacity-40 transition-all hover:text-designColor cursor-pointer duration-300">
+          <SiStreamlit />
+          <TbBrandNetflix />
+        </span>
       </div>
       <div>
         <ul className="flex items-center gap-10">
